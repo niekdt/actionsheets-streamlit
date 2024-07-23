76646e3daca32b8f2fbccf19df1baee50ab73a66
Niek Den Teuling <niekdt@users.noreply.github.com>
2024-07-23 22:12:23 +0200

    docs: update example

--- views/home_view.py
+++ views/home_view.py
@@ -84,7 +84,7 @@ def generate_landing_view():
     Defining a code snippet belonging to the _Create_ section of the respective actionsheet file is as simple as:
     ```toml
     [create.list]
-    what = "Define a list"
+    action = "Define a list"
     code = "x = ['apple', 'pear', 'banana']"
     details = "You can define as many items as you like"
     ```
